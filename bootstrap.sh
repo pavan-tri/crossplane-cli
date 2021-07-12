89944fd8a09cc38b946e37d1fa6ce89a493f9887
pavan-tri <pavan.v@trilogy.com>
2021-07-12 08:10:56 +0000

    arm64

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,30 @@
 set -e
 
 if [[ -z "${RELEASE}" ]]; then
-  RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/crossplane/crossplane-cli/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+  RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/trilogy-group/crossplane-cli/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 fi
 
+detect_arch() {
+  candidate="$(uname -m)"
+
+  if [ "${candidate}" = "aarch64" ]
+  then
+    echo "arm64"
+  elif [ "${candidate}" = "x86_64" ]
+  then
+    echo "amd64"
+  else
+    echo "${candidate}"
+  fi
+}
+
+ARCH="$(detect_arch)"
 PLATFORM="linux"
 if [[ "${OSTYPE}" == "darwin"* ]]; then
   PLATFORM="darwin"
 fi
 
-curl -sL https://github.com/crossplane/crossplane-cli/releases/download/"${RELEASE}"/crossplane-cli_"${RELEASE}"_"${PLATFORM}"_amd64.tar.gz \
+curl -sL https://github.com/trilogy-group/crossplane-cli/releases/download/"${RELEASE}"/crossplane-cli_"${RELEASE}"_"${PLATFORM}"_"${ARCH}".tar.gz \
   | tar -xz -v --strip 1 -C /usr/local/bin 2>&1 \
   | sed 's/x /✓ /g'
 
